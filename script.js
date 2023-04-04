86a957944cc123067e31cf30ac393ef3538119a6
Aaron <aaron.k.ciminelli@gmail.com>
2023-04-04 10:10:53 -1000

    ac

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 // Get Quotes From API
 let apiQuotes = [];
-// show loading
-function loading() {
+
+function showLoadingSpin() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 // hide loading
-function complete() {
+function removeLoadingSpin() {
     if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -20,7 +20,7 @@ function complete() {
 }
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpin();
     // Pick a Random Quote From apiQuotes Array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check If Author Field is Blank and Replace It With 'Unknown'
@@ -37,18 +37,21 @@ function newQuote() {
     }
     // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpin();
 }
 async function getQuotes() {
-    loading();
+    showLoadingSpin();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
         return data;
+        throw new Error('Something went wrong');
+
     } catch (error) {
         // Catch Error Here
+        console.log('whoops, no quote', error);
     }
 }
 // Tweet Quote
